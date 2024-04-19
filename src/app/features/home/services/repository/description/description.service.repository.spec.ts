b29dd3e2421a2a2473c232a40836a5945e80c79d
Catalin <catalinmorar98@yahoo.com>
2024-04-19 03:07:23 +0300

    Fixed tests

--- src/app/features/home/services/repository/description/description.service.repository.spec.ts
+++ src/app/features/home/services/repository/description/description.service.repository.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DescriptionServiceRepository } from './description.service.repository';
+import { ConfigService } from '../../../../../shared/components/configuration/services/configuration.service';
+import { DescriptionAdapter } from '../../../../../shared/model-adapter/description.adapter';
 
 describe('DescriptionServiceRepository', () => {
   let service: DescriptionServiceRepository;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        ConfigService,
+        DescriptionAdapter
+      ]
+    });
     service = TestBed.inject(DescriptionServiceRepository);
   });
 
